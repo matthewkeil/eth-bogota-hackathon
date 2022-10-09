01438c6793e416767944d77e87b6ebc04ff7ab03
AFPedreros <felipe.pedreros94@gmail.com>
2022-10-09 06:23:51 -0500

    frontend

--- packages/frontend/main.js
+++ packages/frontend/main.js
@@ -4,6 +4,10 @@ import { ethers } from "ethers";
 let provider = "";
 const permissionBtn = document.getElementById("permission-btn");
 const connectBtn = document.getElementById("connect-btn");
+const decryptBtn = document.getElementById("decrypt-btn");
+const permissionContainer = document.getElementById("permission-container");
+const decryptContainer = document.getElementById("decrypt-container");
+const passwordLabel = document.getElementById("decrypted-password");
 
 const address = "0x355562132E54364fcbF6f7a61b7a8f4Cb1B3fc30";
 let service = "";
@@ -73,8 +77,47 @@ const prettyAddresses = (address) => {
   return `${address.substr(0, 6)}...${address.substr(address.length - 4)}`;
 };
 
-permissionBtn.addEventListener("click", () => {
-  console.log("clicked");
+permissionBtn.addEventListener("click", () => {});
+
+decryptBtn.addEventListener("click", async () => {
+  const encryptedMessage = document.getElementById("decrypt-input");
+
+  // try {
+  //   const { ethereum } = window;
+  //   const accounts = await ethereum.request({ method: "eth_accounts" });
+  //   const account = accounts[0];
+
+  //   const decryptedMessage = await ethereum.request({
+  //     method: "eth_decrypt",
+  //     params: [encryptedMessage, accounts[0]]
+  //   });
+
+  //   passwordLabel.innerText = decryptedMessage;
+  //   console.log("The decrypted message is:", decryptedMessage);
+  // } catch (error) {
+  //   console.log("error: ", error);
+  // }
+
+  passwordLabel.innerText = encryptedMessage.value; // just for testing
+  encryptedMessage.value = "";
+});
+
+passwordLabel.addEventListener("click", () => {
+  navigator.clipboard.writeText(passwordLabel.innerText);
+  alert("Copied the text: " + passwordLabel.innerText);
+});
+
+document.getElementById("permission-page-btn").addEventListener("click", () => {
+  permissionContainer.classList.remove("hide");
+  permissionContainer.classList.add("display");
+  decryptContainer.classList.add("hide");
+  decryptContainer.classList.remove("display");
+});
+document.getElementById("decrypting-page-btn").addEventListener("click", () => {
+  permissionContainer.classList.add("hide");
+  permissionContainer.classList.remove("display");
+  decryptContainer.classList.remove("hide");
+  decryptContainer.classList.add("display");
 });
 
 main();
