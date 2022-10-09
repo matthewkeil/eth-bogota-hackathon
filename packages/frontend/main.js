8c47fe82f8ddf82cb30e3919bb4b6c490282a5c2
AFPedreros <felipe.pedreros94@gmail.com>
2022-10-08 22:22:51 -0500

    fixing frontend

--- packages/frontend/main.js
+++ packages/frontend/main.js
@@ -1,35 +1,80 @@
 import { SkyBlockContractService } from "../contracts/dist/src/SkyBlockContract";
+import { ethers } from "ethers";
 
+let provider = "";
 const permissionBtn = document.getElementById("permission-btn");
 const connectBtn = document.getElementById("connect-btn");
 
-const getDaoAdminsHtml = () => {
+const address = "0x355562132E54364fcbF6f7a61b7a8f4Cb1B3fc30";
+let service = "";
+
+const main = async () => {
+  if (window.ethereum) {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    service = new SkyBlockContractService({ address });
+  }
+  getDaoAdminsHtml();
+  MetaMaskClientCheck();
+};
+
+const getDaoAdminsHtml = async () => {
+  let html = "";
+  const admins = await service.getAdmins();
+  for (const admin of admins) {
+    let name = await provider.lookupAddress(admin);
+    if (!name) {
+      name = prettyAddresses(admin);
+    }
+    html += `
+      <p>${name}</p>
+    `;
+  }
+
   document.getElementById("dao-member-list").innerHTML = `
   ${document.getElementById("dao-member-list").innerHTML}
+  ${html}
   `;
 };
 
-// async function connect() {
-//   if (typeof window.ethereum !== "undefined") {
-//     try {
-//       await window.ethereum.request({ method: "eth_requestAccounts" });
-//     } catch (error) {
-//       console.log(error);
-//     }
+const MetaMaskClientCheck = async () => {
+  if (isMetaMaskInstalled()) {
+    isWalletConnected();
+  } else {
+    isWalletNotInstall();
+  }
+};
 
-//     connectBtn.innerHTML = "Connected!";
-//   } else {
-//     connectBtn.innerHTML = "Please install Metamask!";
-//   }
-// }
+const isMetaMaskInstalled = () => {
+  const { ethereum } = window;
+  return Boolean(ethereum && ethereum.isMetaMask);
+};
+
+const isWalletConnected = async () => {
+  try {
+    const { ethereum } = window;
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    const account = accounts[0];
+    let name = await provider.lookupAddress(account);
+    if (!name) {
+      name = prettyAddresses(account);
+    }
+    connectBtn.innerText = `${name}`;
+  } catch (error) {
+    console.log("error: ", error);
+  }
+};
+
+const isWalletNotInstall = async () => {
+  connectBtn.innerText = "Install MetaMask!";
+};
+
+const prettyAddresses = (address) => {
+  return `${address.substr(0, 6)}...${address.substr(address.length - 4)}`;
+};
 
 permissionBtn.addEventListener("click", () => {
   console.log("clicked");
-  getDaoAdminsHtml();
 });
-// connectBtn.addEventListener("click", connect);
 
-const address = "0x355562132E54364fcbF6f7a61b7a8f4Cb1B3fc30";
-const service = new SkyBlockContractService({ address });
-service.getAdmins().then(console.log);
-service.getNominees().then(console.log);
+main();
